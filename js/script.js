aba8a9ab3052072106b1fbdfaf807dd60e0a014a
Jack Bott <millerbott@gmail.com>
2020-02-09 03:51:41 -0500

    photo pages

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-/* jshint esversion: 6 */
+/* jshint esversion: 8 */
 
 /* store expanded square id identifier */
 let expanded = null;
@@ -8,9 +8,9 @@ ids = ["info","acacia","solar","guzzy","pru","coliberate","iot","senegal",
   "rtc","india","at"];
 
 /* get page from pages directory */
-const get_page = (page) => {
+const get_page = async (page) => {
   // console.log("get_page");
-  fetch("./pages/" + page + ".html")
+  await fetch("./pages/" + page + ".html")
   .then(response => {
     return response.text();
   })
@@ -20,31 +20,31 @@ const get_page = (page) => {
   .catch((error) => {
     console.warn(error);
   });
+
+  return false;
 };
 
 /* add a link to the indicated id that on click will expand the element to
    nearly the window size and scroll to just above the element */
-const link_expand = (id) => {
+const link_expand = async (id) => {
   // console.log("link_expand");
-  $(document).ready(function() {
-    $("#" + id + "-sq").click(function(e){
-      expand_focus(id, id + "-sq",id + "-sq-title",id + "-sq-hide");
-      e.stopPropagation(); // bubbling propagation by default
-      // animate the transition
-      /* $([document.documentElement, document.body]).animate({
-          scrollTop: $("#" + id + "-sq").offset().top - 20
-      }, 1000); */
-      // don't animate the transition
-      $([document.documentElement, document.body]).scrollTop(
-        $("#" + id + "-sq").offset().top - 20
-      );
-      return false;
-    });
+  $("#" + id + "-sq").click(function(e){
+    expand_focus(id, id + "-sq",id + "-sq-title",id + "-sq-hide");
+    e.stopPropagation(); // bubbling propagation by default
+    // animate the transition
+    /* $([document.documentElement, document.body]).animate({
+        scrollTop: $("#" + id + "-sq").offset().top - 20
+    }, 1000); */
+    // don't animate the transition
+    $([document.documentElement, document.body]).scrollTop(
+      $("#" + id + "-sq").offset().top - 20
+    );
+    return false;
   });
 };
 
 /* shrink all squares back to standard */
-const shrink_focus = () => {
+const shrink_focus = async () => {
   // console.log("shrink_focus");
   // console.log(document.querySelectorAll('[id*=-sq]'));
   let sq = document.querySelectorAll('[id$=-sq]'), i;
@@ -64,6 +64,9 @@ const shrink_focus = () => {
   for (k = 0; k < sq_hide.length; ++k) {
     $(sq_hide[k]).css("display","none");
   }
+  for (let i = 0; i < ids.length; i++) {
+    link_expand(ids[i]);
+  }
   expanded = null;
 };
 
@@ -76,14 +79,8 @@ const get_width = () => {
   return width;
 };
 
-/* function here to expand based on id inputs */
-const expand_focus = (id, id_sq, id_sq_title, id_sq_hide) => {
-  // console.log("expand_focus");
-  shrink_focus();
-  width = get_width();
-  // console.log(document.getElementById(id_sq));
-  // console.log(document.getElementById(id_sq_title));
-  get_page(id);
+const display_swap = async (width, id_sq, id_sq_title, id_sq_hide) => {
+  // console.log("display_swap");
   $("#" + id_sq).css("width",width);
   $("#" + id_sq).css("height","auto");
   // $("#" + id_sq_title).css("width",widthEl);
@@ -96,12 +93,27 @@ const expand_focus = (id, id_sq, id_sq_title, id_sq_hide) => {
 
   expanded = id_sq;
 
+  $("#" + id_sq).unbind();
+};
+
+/* function here to expand based on id inputs */
+const expand_focus = async (id, id_sq, id_sq_title, id_sq_hide) => {
+  // console.log("expand_focus");
+  shrink_focus();
+  width = get_width();
+  // console.log(document.getElementById(id_sq));
+  // console.log(document.getElementById(id_sq_title));
+  await get_page(id);
+  await display_swap(width, id_sq, id_sq_title, id_sq_hide);
+
   // allow browser to redraw - https://stackoverflow.com/a/4575011/6669558
-  setInterval(add_links, 0);
+  // setTimeout(add_links, 10);
+  await add_links();
 
 };
 
 const add_links = () => {
+  // console.log("add_links");
   $("#contact").attr("href","mailto:millerbott@gmail.com");
   $('#contact').unbind().click(function(e) {
     document.location = 'mailto:millerbott@gmail.com';
@@ -154,14 +166,85 @@ const add_links = () => {
   $('#ds1307').unbind().click(function(e) {
     document.location = 'assets/docs/DS1307.pdf';
   });
+  $("#at-trail").attr("href","https://www.appalachiantrail.org/home/explore-the-trail");
+  $('#at-trail').unbind().click(function(e) {
+    document.location = 'https://www.appalachiantrail.org/home/explore-the-trail';
+  });
+
+  // console.log("add_links");
+
+  $("#at-14").attr("src","assets/photos/at/1.jpg");
+  $('.at.next').unbind().click(function(e) {
+    next("at-14");
+    e.stopPropagation(); // bubbling propagation by default
+  });
+  $(".at.back").unbind().click(function(e) {
+    back("at-14");
+    e.stopPropagation(); // bubbling propagation by default
+  });
+
+  $("#india-6").attr("src","assets/photos/india/1.jpg");
+  $('.india.next').unbind().click(function(e) {
+    next("india-6");
+    e.stopPropagation(); // bubbling propagation by default
+  });
+  $(".india.back").unbind().click(function(e) {
+    back("india-6");
+    e.stopPropagation(); // bubbling propagation by default
+  });
+
+  $("#senegal-7").attr("src","assets/photos/senegal/1.jpg");
+  $('.senegal.next').unbind().click(function(e) {
+    next("senegal-7");
+    e.stopPropagation(); // bubbling propagation by default
+  });
+  $(".senegal.back").unbind().click(function(e) {
+    back("senegal-7");
+    e.stopPropagation(); // bubbling propagation by default
+  });
+};
+
+/* flip to the next photo, up to quantity */
+const next = (id) => {
+  let current_src = $("#" + id).attr("src");
+  //console.log(current_src.split("/")[2]); // folder
+  let num = current_src.split("/")[3].split(".")[0];
+  let folder = id.split("-")[0];
+  let quantity = id.split("-")[1];
+  // console.log(num,quantity);
+  if (num == quantity) {
+    $("#" + id).attr("src","assets/photos/" + folder + "/1.jpg");
+  }
+  else {
+    num = parseInt(num) + 1;
+    $("#" + id).attr("src","assets/photos/" + folder + "/" + num + ".jpg");
+    // console.log($("#" + id).attr("src"));
+  }
+};
+
+/* cycle through pictures, number based on id name */
+const back = (id) => {
+  let current_src = $("#" + id).attr("src");
+  let num = current_src.split("/")[3].split(".")[0];
+  let folder = id.split("-")[0];
+  let quantity = id.split("-")[1];
+  if (num == 1) {
+    $("#" + id).attr("src","assets/photos/" + folder + "/" + quantity + ".jpg");
+  }
+  else {
+    num = parseInt(num) - 1;
+    $("#" + id).attr("src","assets/photos/" + folder + "/" + num + ".jpg");
+  }
 };
 
 /* add links to expand list of ids based on form */
-const expand = (sq_list) => {
+const expand = async (sq_list) => {
   // console.log("expand");
-  for (let i = 0; i < sq_list.length; i++) {
-    link_expand(sq_list[i]);
-  }
+  $(document).ready(function() {
+    for (let i = 0; i < sq_list.length; i++) {
+      link_expand(sq_list[i]);
+    }
+  });
 };
 
 /* add on click to background that shrinks elements to standard */
