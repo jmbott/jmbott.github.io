059b3861cfdcc5306132f1d45f2b9cbefc5a5899
Jack Bott <millerbott@gmail.com>
2020-02-09 23:37:44 -0500

    update pages etc.

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ let expanded = null;
 ids = ["info","acacia","solar","guzzy","pru","coliberate","iot","senegal",
   "rtc","india","at"];
 
-/* get page from pages directory */
+/* get page/project content from pages directory */
 const get_page = async (page) => {
   // console.log("get_page");
   await fetch("./pages/" + page + ".html")
@@ -20,8 +20,18 @@ const get_page = async (page) => {
   .catch((error) => {
     console.warn(error);
   });
-
   return false;
+};
+
+const scroll = (id) => {
+  // animate the transition
+  /* $([document.documentElement, document.body]).animate({
+      scrollTop: $("#" + id + "-sq").offset().top - 20
+  }, 1000); */
+  // don't animate the transition
+  $('html, body').scrollTop(
+    $("#" + id + "-sq").position().top - 10
+  );
 };
 
 /* add a link to the indicated id that on click will expand the element to
@@ -29,16 +39,9 @@ const get_page = async (page) => {
 const link_expand = async (id) => {
   // console.log("link_expand");
   $("#" + id + "-sq").click(function(e){
-    expand_focus(id, id + "-sq",id + "-sq-title",id + "-sq-hide");
     e.stopPropagation(); // bubbling propagation by default
-    // animate the transition
-    /* $([document.documentElement, document.body]).animate({
-        scrollTop: $("#" + id + "-sq").offset().top - 20
-    }, 1000); */
-    // don't animate the transition
-    $([document.documentElement, document.body]).scrollTop(
-      $("#" + id + "-sq").offset().top - 20
-    );
+    expand_focus(id, id + "-sq",id + "-sq-title",id + "-sq-hide");
+    // scroll(id);
     return false;
   });
 };
@@ -64,10 +67,10 @@ const shrink_focus = async () => {
   for (k = 0; k < sq_hide.length; ++k) {
     $(sq_hide[k]).css("display","none");
   }
-  for (let i = 0; i < ids.length; i++) {
-    link_expand(ids[i]);
-  }
-  expanded = null;
+  // for (let i = 0; i < ids.length; i++) {
+  //   link_expand(ids[i]);
+  // }
+  expanded = null; // reset what square is expanded
 };
 
 /* get the width to set elements to */
@@ -79,6 +82,7 @@ const get_width = () => {
   return width;
 };
 
+/* setup css and mark expanded unit */
 const display_swap = async (width, id_sq, id_sq_title, id_sq_hide) => {
   // console.log("display_swap");
   $("#" + id_sq).css("width",width);
@@ -90,10 +94,9 @@ const display_swap = async (width, id_sq, id_sq_title, id_sq_hide) => {
   $("#" + id_sq_hide).css("margin","auto");
   $("#" + id_sq_hide).css("width","auto");
   // $("#" + id_sq_hide).addClass("hide-id"); // cleaner?
+  // $("#" + id_sq).unbind();
 
   expanded = id_sq;
-
-  $("#" + id_sq).unbind();
 };
 
 /* function here to expand based on id inputs */
@@ -101,19 +104,20 @@ const expand_focus = async (id, id_sq, id_sq_title, id_sq_hide) => {
   // console.log("expand_focus");
   shrink_focus();
   width = get_width();
-  // console.log(document.getElementById(id_sq));
-  // console.log(document.getElementById(id_sq_title));
+
+  /* get page, load, setup, add links */
   await get_page(id);
   await display_swap(width, id_sq, id_sq_title, id_sq_hide);
-
-  // allow browser to redraw - https://stackoverflow.com/a/4575011/6669558
-  // setTimeout(add_links, 10);
   await add_links();
-
+  scroll(id);
 };
 
+/* add links after render, use click events, href for clarity */
 const add_links = () => {
   // console.log("add_links");
+  $(".project").unbind().click(function(e){
+    e.stopPropagation(); // bubbling propagation by default
+  });
   $("#contact").attr("href","mailto:millerbott@gmail.com");
   $('#contact').unbind().click(function(e) {
     document.location = 'mailto:millerbott@gmail.com';
@@ -126,6 +130,10 @@ const add_links = () => {
   $('#linkedin').unbind().click(function(e) {
     document.location = 'https://www.linkedin.com/in/jmb2341';
   });
+  // $("#stackoverflow").attr("href","https://stackoverflow.com/users/6669558/jmb2341");
+  // $('#stackoverflow').unbind().click(function(e) {
+  //   document.location = 'https://stackoverflow.com/users/6669558/jmb2341';
+  // });
   // $("#resume").attr("href","asssets/docs/resume.pdf");
   // $('#resume').unbind().click(function(e) {
   //   document.location = 'asssets/docs/resume.pdf';
@@ -150,6 +158,10 @@ const add_links = () => {
   $('#ti-pru').unbind().click(function(e) {
     document.location = 'https://processors.wiki.ti.com/index.php/PRU-ICSS';
   });
+  $("#lab-site").attr("href","https://mplab.ee.columbia.edu/");
+  $('#lab-site').unbind().click(function(e) {
+    document.location = 'https://mplab.ee.columbia.edu/';
+  });
   // $("#pru-src").attr("href","");
   // $('#pru-src').unbind().click(function(e) {
   //   document.location = '';
@@ -171,11 +183,10 @@ const add_links = () => {
     document.location = 'https://www.appalachiantrail.org/home/explore-the-trail';
   });
 
-  // console.log("add_links");
-
+  // photo page carousels
   $("#at-14").attr("src","assets/photos/at/1.jpg");
   $('.at.next').unbind().click(function(e) {
-    next("at-14");
+    next("at-14"); // id and # of photos
     e.stopPropagation(); // bubbling propagation by default
   });
   $(".at.back").unbind().click(function(e) {
@@ -185,7 +196,7 @@ const add_links = () => {
 
   $("#india-6").attr("src","assets/photos/india/1.jpg");
   $('.india.next').unbind().click(function(e) {
-    next("india-6");
+    next("india-6"); // id and # of photos
     e.stopPropagation(); // bubbling propagation by default
   });
   $(".india.back").unbind().click(function(e) {
@@ -195,7 +206,7 @@ const add_links = () => {
 
   $("#senegal-7").attr("src","assets/photos/senegal/1.jpg");
   $('.senegal.next').unbind().click(function(e) {
-    next("senegal-7");
+    next("senegal-7"); // id and # of photos
     e.stopPropagation(); // bubbling propagation by default
   });
   $(".senegal.back").unbind().click(function(e) {
@@ -222,7 +233,7 @@ const next = (id) => {
   }
 };
 
-/* cycle through pictures, number based on id name */
+/* cycle back through pictures, number based on id name */
 const back = (id) => {
   let current_src = $("#" + id).attr("src");
   let num = current_src.split("/")[3].split(".")[0];
@@ -282,7 +293,7 @@ const correct_width = () => {
   // console.log("resize",expanded);
   if (expanded != null) {
     width = get_width();
-    console.log(width);
+    // console.log(width);
     $("#" + expanded).css("width",width);
   }
 };
